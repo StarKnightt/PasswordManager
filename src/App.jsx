ab8f1a9048d2c0c14dcc7bcebe5926cb9b749b51
StarKnightt <prasenjitt4e@gmail.com>
2024-05-14 23:33:07 +0530

    added the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './Components/Navbar'
-import Manager from './Components/Manager'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Manager from "./Components/Manager";
+import Footer from "./Components/Footer";
 
 function App() {
-
   return (
     <>
-    <Navbar/>
-    <Manager />
+      <Navbar />
+
+      <Manager />
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-slate-800 text-white flex flex-col justify-center items-center fixed bottom-0 w-full">
+      <div className="logo font-bold text-white text-2xl ">
+        <span className="text-green-400"> &lt;</span>
+        Lock<span className="text-green-500"> Magic/&gt;</span>
+      </div>
+      <div className="flex justify-center items-center">
+        Created with <img className="w-7 mx-2" src="icons/heart.png" alt="" />{" "}
+        by Prasen
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
