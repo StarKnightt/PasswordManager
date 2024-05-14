a71e2fd2adea164901bbd5b76216d45b503475a3
StarKnightt <prasenjitt4e@gmail.com>
2024-05-14 21:20:46 +0530

    improved the Navbar and some Ui fixes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,20 +5,23 @@ const Navbar = () => {
     <nav className='bg-slate-800 text-white'>
       <div className="mycontainer flex justify-between items-center px-4 py-5 h-14 ">
 
-      <div className="logo font-bold text-white text-2xl">
-        <span className='text-green-400'> &lt;</span>
-        Lock<span className='text-green-500'>  Magic/&gt;</span>
+        <div className="logo font-bold text-white text-2xl">
+          <span className='text-green-400'> &lt;</span>
+          Lock<span className='text-green-500'>  Magic/&gt;</span>
         </div>
-      <ul>
-        <li className='flex gap-4'>
-          <a className='hover:font-bol' href="">Home</a>
-          <a className='hover:font-bol' href="">Contact</a>
-          <a className='hover:font-bol' href="">About</a>
-        </li>
-      </ul>
-        
+        {/* <ul>
+          <li className='flex gap-4'>
+            <a className='hover:font-bol' href="">Home</a>
+            <a className='hover:font-bol' href="">Contact</a>
+            <a className='hover:font-bol' href="">About</a>
+          </li>
+        </ul> */}
+        <button className='flex items-center justify-center h-11 px-4 mx-2 rounded-full border border-gray-300 bg-transparent text-white hover:bg-gray-700 hover:border-gray-500'>
+          <img className='w-6 h-6 mr-2 invert' src="/icons/github.svg" alt="GitHub Logo" />
+          <span className='font-semibold'>GitHub</span>
+        </button>
       </div>
-        </nav>
+    </nav>
   )
 }
 
