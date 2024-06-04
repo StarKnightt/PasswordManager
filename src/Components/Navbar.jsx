ddbdb90c9ecbcb12e8bd6fe468a640b7e3e75a0d
StarKnightt <prasenjitt4e@gmail.com>
2024-06-04 16:12:04 +0530

    added the link in github button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         </ul> */}
         <button className='flex items-center justify-center h-11 px-4 mx-2 rounded-full border border-gray-300 bg-transparent text-white hover:bg-gray-700 hover:border-gray-500'>
           <img className='w-6 h-6 mr-2 invert' src="/icons/github.svg" alt="GitHub Logo" />
-          <span className='font-semibold'>GitHub</span>
+      <a href="https://github.com/StarKnightt/PasswordManager"><span className='font-semibold'>GitHub</span></a>
         </button>
       </div>
     </nav>
